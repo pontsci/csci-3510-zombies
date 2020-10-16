1931da60b1cf6635bdb4e659a0368d354045519a
Mason Flemingm <mgfleming@unomaha.edu>
2020-10-16 17:00:44 -0500

    script completion and prefab creation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -21,6 +21,47 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            PlaySound();
+            animator.SetFloat("forward", 1.0f);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+            groan.Stop();
+            animator.SetFloat("forward", 0.0f);
+        }
+
+        if(distance <= agent.stoppingDistance)
+        {
+            animator.SetFloat("forward", 0.0f);
+            FaceTarget();
+        }
+
         
+    }
+    void PlaySound()
+    {
+        if (!groan.isPlaying)
+        {
+            groan.Play();
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime*5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
