b67a86c5886598a2db426713a77a24eaa24c1f08
Mason Flemingm <mgfleming@unomaha.edu>
2020-10-16 16:48:28 -0500

    initialize EnemyController.cs

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 8f;
+
+    Transform target;
+    NavMeshAgent agent;
+    AudioSource groan;
+    Animator animator;
+
+    void Start()
+    {
+        target = PlayerManager.Instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+        groan = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
