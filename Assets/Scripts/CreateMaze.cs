5ffafb830885278e4f46003586267d20f563d23e
Mason Flemingm <mgfleming@unomaha.edu>
2020-10-16 15:43:38 -0500

    initial commit

---
+++ Assets/Scripts/CreateMaze.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateMaze : MonoBehaviour
+{   
+    public Material floorMaterial;
+    public Material wallMaterial;
+
+    GameObject maze;
+    string mazeTag;
+
+    void Start()
+    {
+        mazeTag = "Maze";
+
+        // Transform is always added to the GameObject that is being created. 
+        // The creation of a GameObject with no script arguments will add the Transform but nothing else.
+        maze = new GameObject("MyMaze");
+        maze.tag = mazeTag; // assumes tag exists
+
+        CreateMazeObject();
+    }
+
+    void CreateMazeObject ()
+    {
+        // lower left reference point
+        float x = 0; 
+        float y = 0; 
+        float z = 0;
+
+        string floor = "floor";
+        string wall  = "wall";
+
+        // floor
+        CreateCubeWithLowerLeftRect(new Vector3(12,1,12), new Vector3(x,y,z), floorMaterial, floor); // rgba
+
+        // walls
+        CreateCubeWithLowerLeftRect(new Vector3(3, 1, 1), new Vector3(x   , y+1, z+ 1), wallMaterial, wall);
+        CreateCubeWithLowerLeftRect(new Vector3(1, 1, 4), new Vector3(x+ 4, y+1, z   ), wallMaterial, wall);
+        CreateCubeWithLowerLeftRect(new Vector3(1, 1, 3), new Vector3(x+ 6, y+1, z+ 1), wallMaterial, wall);
+        CreateCubeWithLowerLeftRect(new Vector3(4, 1, 1), new Vector3(x+ 7, y+1, z+ 1), wallMaterial, wall);
+                                                                                                    
+        CreateCubeWithLowerLeftRect(new Vector3(2, 1, 1), new Vector3(x+ 1, y+1, z+ 3), wallMaterial, wall);
+        CreateCubeWithLowerLeftRect(new Vector3(1, 1, 5), new Vector3(x+ 8, y+1, z+ 3), wallMaterial, wall);
+        CreateCubeWithLowerLeftRect(new Vector3(2, 1, 1), new Vector3(x+10, y+1, z+ 3), wallMaterial, wall);
+                                                                                                    
+        CreateCubeWithLowerLeftRect(new Vector3(1, 1, 2), new Vector3(x+ 2, y+1, z+ 4), wallMaterial, wall);
+        CreateCubeWithLowerLeftRect(new Vector3(1, 1, 6), new Vector3(x+10, y+1, z+ 4), wallMaterial, wall);
+                                                                                                    
+        CreateCubeWithLowerLeftRect(new Vector3(1, 1, 2), new Vector3(x   , y+1, z+ 5), wallMaterial, wall);
+        CreateCubeWithLowerLeftRect(new Vector3(4, 1, 1), new Vector3(x+ 3, y+1, z+ 5), wallMaterial, wall);
+                                                                                                    
+        CreateCubeWithLowerLeftRect(new Vector3(1, 1, 3), new Vector3(x+ 3, y+1, z+ 6), wallMaterial, wall);
+                                                                                                    
+        CreateCubeWithLowerLeftRect(new Vector3(4, 1, 1), new Vector3(x+ 5, y+1, z+ 7), wallMaterial, wall);
+                                                                                                    
+        CreateCubeWithLowerLeftRect(new Vector3(2, 1, 1), new Vector3(x+ 1, y+1, z+ 8), wallMaterial, wall);
+        CreateCubeWithLowerLeftRect(new Vector3(1, 1, 4), new Vector3(x+ 6, y+1, z+ 8), wallMaterial, wall);
+                                                                                                    
+        CreateCubeWithLowerLeftRect(new Vector3(1, 1, 2), new Vector3(x+ 1, y+1, z+ 9), wallMaterial, wall);
+        CreateCubeWithLowerLeftRect(new Vector3(3, 1, 1), new Vector3(x+ 8, y+1, z+ 9), wallMaterial, wall);
+                                                                                                    
+        CreateCubeWithLowerLeftRect(new Vector3(3, 1, 1), new Vector3(x+ 2, y+1, z+10), wallMaterial, wall);
+    }
+
+    void CreateCubeWithLowerLeftRect( Vector3 size, Vector3 position, Material material, string name)
+    {
+        float xCenter = position.x;
+        float yCenter = position.y;
+        float zCenter = position.z;
+
+        float xSize = size.x;
+        float ySize = size.y;
+        float zSize = size.z;
+
+        // lower left reference point
+        float x = xCenter + xSize / 2; 
+        float y = yCenter + ySize / 2; 
+        float z = zCenter + zSize / 2; 
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.localScale = size;
+        cube.transform.position = new Vector3(x,y,z);
+        cube.GetComponent<Renderer>().material = material;
+
+        cube.name = name;
+        //cube.tag = mazeTag;
+
+        cube.transform.SetParent(maze.transform);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float baseSpeed;
+    public float runningAmplifier;
+
+    public bool lockCursor;
+
+    private CharacterController characterController;
+    private Animator animator;
+
+    private float airTime;
+    private float gravity = -9.81f;
+
+    private PlayerMovementInfo playerMovement;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+
+        playerMovement = new PlayerMovementInfo();
+        playerMovement.baseSpeed = baseSpeed;
+        playerMovement.runningAmplifier = runningAmplifier;
+
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        airTime = 0;
+    }
+
+    void Update()
+    {
+        ProcessInput();
+        PerformBlendTreeAnimation();
+        CalculateDirectionAndDistance();
+        PerformPhysicalMovement();
+        RotatePlayerWithCamera();
+    }
+
+    public void ProcessInput()
+    {
+        playerMovement.leftAndRight = Input.GetAxis("Horizontal");//A, D
+        playerMovement.forwardsAndBackwords = Input.GetAxis("Vertical");//W, S, -1 to 1
+
+        playerMovement.movingForwards = playerMovement.forwardsAndBackwords > 0.0f;
+        playerMovement.movingBackwards = playerMovement.forwardsAndBackwords < 0.0f;
+
+        bool running = (playerMovement.movingForwards && Input.GetKey(KeyCode.LeftShift))
+                    || (!playerMovement.movingBackwards
+                        && (playerMovement.leftAndRight > 0.0f || playerMovement.leftAndRight < 0.0f));
+
+        if (running)
+        {
+            playerMovement.speed = playerMovement.baseSpeed * playerMovement.runningAmplifier;
+        }
+        else
+        {
+            playerMovement.speed = playerMovement.baseSpeed;
+            playerMovement.forwardsAndBackwords = playerMovement.forwardsAndBackwords / 2.0f;
+        }
+    }
+
+    public void PerformBlendTreeAnimation()
+    {
+        float leftAndRight = playerMovement.leftAndRight;
+
+        if (playerMovement.movingBackwards)
+        {
+            leftAndRight = 0.0f;
+        }
+
+        animator.SetFloat("leftAndRight", leftAndRight);
+        animator.SetFloat("forwardsAndBackwards", playerMovement.forwardsAndBackwords);
+    }
+
+    public void CalculateDirectionAndDistance()
+    {
+        Vector3 moveDirectionForward = transform.forward * playerMovement.forwardsAndBackwords;
+        Vector3 moveDirectionSide = transform.right * playerMovement.leftAndRight;
+
+        playerMovement.direction = moveDirectionForward + moveDirectionSide;
+        playerMovement.normalizedDirection = playerMovement.direction.normalized;
+
+        playerMovement.distance = playerMovement.normalizedDirection * playerMovement.speed * Time.deltaTime;
+
+        GroundPlayer();
+    }
+
+    public void PerformPhysicalMovement()
+    {
+        characterController.Move(playerMovement.distance);
+    }
+
+    public void GroundPlayer()
+    {
+        if (characterController.isGrounded)
+        {
+            airTime = 0;
+        }
+        else
+        { //falling (or jumping) 
+            airTime += Time.deltaTime;
+            Vector3 direction = playerMovement.normalizedDirection;
+
+            direction.y += 0.5f * gravity * airTime;
+
+            playerMovement.normalizedDirection = direction;
+            playerMovement.distance = playerMovement.normalizedDirection * airTime;
+        }
+
+
+
+    }
+
+    public void RotatePlayerWithCamera()
+    {
+        Vector3 rotation;
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            return;
+        }
+        else
+        {
+            rotation = Camera.main.transform.eulerAngles;
+            rotation.x = 0;
+            rotation.z = 0;
+
+            transform.eulerAngles = rotation;
+        }
+    }
+}
