38da6c6fefb0889d1c549dcc7c9ae6e2dd0eb03f
Mason Flemingm <mgfleming@unomaha.edu>
2020-10-16 16:42:53 -0500

    blend trees initial creation

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+    public static PlayerManager Instance;
+    void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    public GameObject player;
+}
